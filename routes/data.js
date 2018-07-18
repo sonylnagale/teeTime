6dd6eae3784464344542822bc0a6f7f0348b98c5
Kevin ODea <kodea@hubspot.com>
2018-07-18 16:46:50 -0400

    push

--- routes/data.js
+++ routes/data.js
@@ -33,7 +33,8 @@ router.get('/', function(req, res){
             var person = parsed["canonical-vid"];
             var dealURL = 'https://api.hubapi.com/deals/v1/deal/associated/contact/' + person +'/paged?hapikey=' + process.env.apikey +'&includeAssociations=true&limit=10&properties=dealname&properties=first_name'
             https.get(url, function(response) {
-			console.log(response);
+				console.log(response);
+			};
             // callback({
             //     email: parsed.email,
             // });
