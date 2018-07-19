13dc7116dcd7529d9bd088cbe3c0402b2113187c
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2018-07-19 13:43:32 -0400

    With data retrieval

--- routes/data.js
+++ routes/data.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var https = require('https');
+var dotenv = require('dotenv').load();
 /* GET home page. */
+
+console.log('key', process.env.apikey);
 
 router.get('/', function(req, res){
 	// var obj = {};
 	// obj.title = 'title';
 	// obj.data = 'data';
-	
+
 	// console.log('params: ' + JSON.stringify(req.params));
 	// console.log('body: ' + JSON.stringify(req.body));
 	// console.log('query: ' + JSON.stringify(req.query));
 
-	// request.get('https://api.hubapi.com/contacts/v1/contact/email/kodea@hubspot.com/profile?hapikey=' + process.env.apikey +'&property=email', 
+	// request.get('https://api.hubapi.com/contacts/v1/contact/email/kodea@hubspot.com/profile?hapikey=' + process.env.apikey +'&property=email',
 	// 	function(response) {
 	// 		console.log(response);
 	// 	});
@@ -21,42 +24,45 @@ router.get('/', function(req, res){
 
 	https.get(url, function(response) {
         // Continuously update stream with data
-        
+
        var body = '';
         response.on('data', function(d) {
             body += d;
         });
         response.on('end', function() {
 
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            var person = parsed["canonical-vid"];
-            var dealURL = 'https://api.hubapi.com/deals/v1/deal/associated/contact/' + person +'/paged?hapikey=' + process.env.apikey +'&includeAssociations=true&limit=10&properties=dealname&properties=first_name'
-            https.get(dealURL, function(response) {
-				var body = '';
+          // Data reception is done, do whatever with it!
+          var parsed = JSON.parse(body);
+          var person = parsed["canonical-vid"];
+          var dealURL = 'https://api.hubapi.com/deals/v1/deal/associated/contact/' + person +'/paged?hapikey=' + process.env.apikey +'&includeAssociations=true&limit=10&properties=dealname&properties=first_name'
+          https.get(dealURL, function(response) {
+						var body = '';
         		response.on('data', function(d) {
             		body += d;
         		});
+
         		response.on('end', function() {
-        			var dealBody = JSON.parse(body);
-        			for (key of Object.keys(dealBody)) {
-        				if (key == "properties") {
-        					var properties = dealBody[key];
+        			var dealBody = JSON.parse(body).deals;
+
+        			for (var i = 0; i < dealBody.length; i++) {
+        				if (dealBody[i].properties) {
+        					var properties = dealBody[i].properties;
+									if (properties.first_name) {
+										console.log(properties.first_name.value);
+									}
         				}
-        				for (key of Object.keys(properties)) {
-        					console.log(properties[key]);
-        				};
-        				// console.log(key, dealBody[key]);
+        				// for (key of Object.keys(properties)) {
+        				// 	console.log(properties[key]);
+        				// };
         			};
+						});
 					// res.send(body.properties.first_name);
 				});
             // callback({
             //     email: parsed.email,
             // });
-        	});
     	});
-    });
-
+		});
 	// $.ajax({
 	//   type: "GET",
 	//   url: "https://accesscontrolalloworiginall.herokuapp.com/https://api.hubapi.com/contacts/v1/contact/email/kodea@hubspot.com/profile?hapikey=APIKEYHERE&property=email",
@@ -64,12 +70,10 @@ router.get('/', function(req, res){
 	//   	console.log(response);
 	//   },
 	// });
-	
+
 // 	res.header('Content-type','application/json');
 // 	res.header('Charset','utf8');
 // 	res.send(req.query.callback + '('+ JSON.stringify(obj) + ');');
 });
 
 module.exports = router;
-
-
